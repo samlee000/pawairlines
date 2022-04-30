7ddc38659eb3a8462ba469cf31d517b52dab0492
Nick-Robert20 <nick_robert20@tamu.edu>
2022-04-30 12:55:32 -0500

    Added needed tables and admin bill files

---
+++ client/src/components/sqlComponents/InputBill.js
@@ -0,0 +1,5 @@
+import React, { Fragment, useState } from "react";
+
+const InputBill = () => {
+    
+}
--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,23 @@
-CREATE DATABASE pawsairline;
-
-CREATE TABLE flights(
-    flight_id SERIAL PRIMARY KEY, 
+CREATE TABLE flights (
+    flight_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
+);
+
+CREATE TABLE billing (
+    bill_id SERIAL PRIMARY KEY,
+    user_id SERIAL,
+    routing_no SERIAL,
+    subtotal DECIMAL(50, 2),
+    total DECIMAL(50, 2)
+);
+
+CREATE TABLE tickets (
+    ticket_id SERIAL PRIMARY KEY,
+    flight_id SERIAL,
+    user_id SERIAL,
+    bill_id SERIAL,
+    seat_number int,
+    class VARCHAR(30),
+    pets_carry_on boolean,
+    price DECIMAL(50, 2)
 );
